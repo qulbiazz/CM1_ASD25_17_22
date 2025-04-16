883c62270f68935ee54a7da56e8bd5e96ad2601e
muhammadhasbiashiddiqi <muhammadhasbiasidiki175@gmail.com>
2025-04-16 09:53:21 +0700

    bank

--- CM1_ASD25_17_22/src/Bank.java
+++ CM1_ASD25_17_22/src/Bank.java
@@ -1,4 +1,22 @@
 public class Bank {
+    String noRekening;
+    String nama;
+    String namaibu;
+    String noHp;
+    String email;
 
-    
+    public Bank() {
+    }
+    public Bank(String noRekening, String nama, String namaibu, String noHp, String email) {
+        this.noRekening = noRekening;
+        this.nama = nama;
+        this.namaibu = namaibu;
+        this.noHp = noHp;
+        this.email = email;
+    }
+    void tampilDataNorek() {
+        System.out.printf("%s\t%s\t%.1f\t\t%.1f\t\t%.1f", noRekening, nama, namaibu, noHp, email);
+    }
+
 }
+    
